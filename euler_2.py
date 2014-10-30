96d4df3b794b557be7352c9168c09ba3f2984e34
Brian McDonald <bmcdonald916@gmail.com>
2014-10-30 09:11:03 -0400

    Started doing some in python

---
+++ euler_2.py
@@ -0,0 +1,11 @@
+# Even Fibonacci numbers
+
+def fibonacci(maximum):
+    first = 1
+    second = 2
+    while first < maximum:
+        yield first
+        first, second = second, first + second
+
+if __name__ == "__main__":
+    print sum((i for i in fibonacci(4000000) if i % 2 == 0))
